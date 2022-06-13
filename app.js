05eff0f94567584f5378bf5e8689d67e0f566447
Célio barros <celiosb86@gmail.com>
2022-06-13 11:58:21 -0300

    alterei o arquivo do server

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const http = require('http')
 
-
-
+//https://es.wikipedia.org/wiki/Anexo:C%C3%B3digos_de_estado_HTTP
 
 http
   .createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-
+    console.log('Servidor ON')
     switch (req.url) {
       case '/':
         res.end('Home')
@@ -18,4 +17,4 @@ http
         res.end('Página Não encontrada')
     }
   })
-  .listen(3030, 'localhost')
+  .listen(3030, 'localhost') //como o server é aqui mesmo no computador, o padrão é o localhost, mesmo se não indicarmos a função http, ira apontar para o localhost.
